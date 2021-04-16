d32614fca6d8d32ae0843cbcd2c706c8a8559c78
nytouu <capitaine7312@gmail.com>
2021-04-16 19:28:32 +0200

    fixed shift k not switching to prev buffer

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,13 +1,3 @@
-vim.cmd("nnoremap <silent> gd <cmd>lua vim.lsp.buf.definition()<CR>")
-vim.cmd("nnoremap <silent> gD <cmd>lua vim.lsp.buf.declaration()<CR>")
-vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
-vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
-vim.cmd("nnoremap <silent> ca :Lspsaga code_action<CR>")
-vim.cmd("nnoremap <silent> K :Lspsaga hover_doc<CR>")
-vim.cmd("nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
-vim.cmd("nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
-vim.cmd('command! -nargs=0 LspVirtualTextToggle lua require("lsp/virtual_text").toggle()')
-
 vim.lsp.protocol.CompletionItemKind = {
     "   (Text) ",
     "   (Method)",
