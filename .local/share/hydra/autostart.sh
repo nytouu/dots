6c3ddc3fc689d5f10c997efd1e68933a50cc1e36
nytouu <capitaine7312@gmail.com>
2021-04-18 16:13:16 +0200

    cleaned up some dots

--- .local/share/hydra/autostart.sh
+++ .local/share/hydra/autostart.sh
@@ -6,7 +6,7 @@ dunst &
 mpcscroll &
 hydrablocks &
 picom --experimental-backends &
-xwallpaper --zoom /home/nytou/pics/wallpapers/pawel-czerwinski-y_rotated.jpg &
+xwallpaper --zoom /home/nytou/pics/wallpapers/sakura.jpg &
 # xwinwrap -g 1920x1080+0+0 -b -ov -fs -nf -ni -fdt -d -- mpv -wid WID /home/nytou/pics/gifs/bg.gif --loop --keepaspect=no &
 discord &
 pkill -RTMIN+10 hydrablocks &
