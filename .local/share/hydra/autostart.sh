0abb042183559e085c245ea29590006561ce87ef
nytouu <capitaine7312@gmail.com>
2021-04-20 19:37:32 +0200

    workaround for hydrablocks volume block

--- .local/share/hydra/autostart.sh
+++ .local/share/hydra/autostart.sh
@@ -9,4 +9,4 @@ picom --experimental-backends &
 xwallpaper --zoom /home/nytou/pics/wallpapers/sakura.jpg &
 # xwinwrap -g 1920x1080+0+0 -b -ov -fs -nf -ni -fdt -d -- mpv -wid WID /home/nytou/pics/gifs/bg.gif --loop --keepaspect=no &
 discord &
-pkill -RTMIN+10 hydrablocks &
+sleep 3 && pkill -RTMIN+10 hydrablocks &
