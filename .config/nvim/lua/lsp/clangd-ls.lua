7c0de97a98f6b1aa0d407e6bcdeac9a3e51de0c7
nytouu <capitaine7312@gmail.com>
2021-04-24 19:14:05 +0200

    removed virtual text for clangd

--- .config/nvim/lua/lsp/clangd-ls.lua
+++ .config/nvim/lua/lsp/clangd-ls.lua
@@ -2,7 +2,7 @@ require'lspconfig'.clangd.setup {
     cmd = {DATA_PATH .. "/lspinstall/cpp/clangd/bin/clangd"},
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = true,
+            virtual_text = false,
             signs = true,
             underline = true,
             update_in_insert = true
