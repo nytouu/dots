2ec6029a76902244efdcedf6d3c13d7327f5b1ef
nytouu <capitaine7312@gmail.com>
2021-06-13 02:25:14 +0200

    nvim - hydra art on dashboard rip froge

--- config/nvim/lua/dashboard.lua
+++ config/nvim/lua/dashboard.lua
@@ -1,6 +1,21 @@
-vim.g.dashboard_custom_header = {
+vim.g.dashboard_default_executive = 'telescope'
+
+vim.g.dashboard_custom_section = {
+    a = {description = {'  Find File        spc f f'}, command = 'Telescope find_files'},
+    b = {description = {'  New File         spc f n'}, command = ':DashboardNewFile'},
+    c = {description = {'  Recent Files     spc f h'}, command = 'Telescope oldfiles'},
+    d = {description = {'  Live Grep        spc f w'}, command = 'Telescope live_grep'},
+    e = {description = {'漣 Settings         spc f s'}, command = ':e $HOME/.config/nvim/lua/settings.lua'},
+    f = {description = {'  Quit             spc q q'}, command = ':q'}
+}
+
+vim.g.dashboard_custom_footer = {'hello(world);'}
+
+vim.g.dashboard_custom_header =
+--[[
+{
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠤⠴⠶⠶⠤⣄⣀⣀⣀⣀⡴⠛⡶⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
-'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠴⢊⠕⠊⠉⠉⠉⠢⡘⠀⠀⠀⠀⠀⠀⠑⣔⣚⣣⡀⣀⠀⠀⠀⠀⠀⠀⠀⠀',
+ '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠴⢊⠕⠊⠉⠉⠉⠢⡘⠀⠀⠀⠀⠀⠀⠑⣔⣚⣣⡀⣀⠀⠀⠀⠀⠀⠀⠀⠀',
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠴⠖⠋⠁⠀⠀⠻⣟⣋⣙⠻⠥⠞⠁⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⢳⠀⠀⠀⠀⠀⠀⠀',
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠖⠛⠉⠀⢀⡔⠋⠉⡤⠤⠔⠒⠒⠒⠐⠚⠛⠛⠛⠛⠓⠒⠒⠲⠤⠤⠤⠤⠔⡲⠚⠀⠀⠀⠀⠀⠀⠀',
 '⠀⠀⠀⠀⠀⠀⠀⣠⡴⠚⠉⠀⠀⠀⠀⠀⠈⠳⠦⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀',
@@ -16,16 +31,59 @@ vim.g.dashboard_custom_header = {
 '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                   ',
 '                     big froge                    '
 }
+]]
 
-vim.g.dashboard_default_executive = 'telescope'
-
-vim.g.dashboard_custom_section = {
-    a = {description = {'  Find File        spc f f'}, command = 'Telescope find_files'},
-    b = {description = {'  New File         spc f n'}, command = ':DashboardNewFile'},
-    c = {description = {'  Recent Files     spc f h'}, command = 'Telescope oldfiles'},
-    d = {description = {'  Live Grep        spc f w'}, command = 'Telescope live_grep'},
-    e = {description = {'漣 Settings         spc f s'}, command = ':e $HOME/.config/nvim/lua/settings.lua'},
-    f = {description = {'  Quit             spc q q'}, command = ':q'}
+{
+'  ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆        ',
+'   ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦      ',
+'         ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄    ',
+'          ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀ ⠢⣀⡀⠈⠙⠿⠄   ',
+'         ⢠⣿⣿⣿⠈   ⠈⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀  ',
+'  ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄ ',
+' ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾   ',
+'⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄ ',
+'⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄',
+'     ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆   ⠁',
+'      ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃    ',
+'                                 ',
+'             hydra               '
 }
 
-vim.g.dashboard_custom_footer = {'hello(world);'}
+--[[
+{
+    "                                                                    ",
+    "            :h-                                  Nhy`               ",
+    "           -mh.                           h.    `Ndho               ",
+    "           hmh+                          oNm.   oNdhh               ",
+    "          `Nmhd`                        /NNmd  /NNhhd               ",
+    "          -NNhhy                      `hMNmmm`+NNdhhh               ",
+    "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+    "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+    "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+    "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+    " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+    " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+    " hd+` -NNNy`./dNNNNNhhhh+-://///   -+ooo:`  ::-:+////++///:`        ",
+    " /Nmhs+oss-:++/dNNNmhho:--::///   /mmmmmmo  ../-///++///////.       ",
+    "  oNNdhhhhhhhs//osso/:---:::///   /myyyyso  ..o+-//////////:/.      ",
+    "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+    "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+    "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+    "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+    "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+    "       //+++//++++++////+++///::--                 .::::-------::   ",
+    "       :/++++///////////++++//////.                -:/:----::../-   ",
+    "       -/++++//++///+//////////////               .::::---:::-.+`   ",
+    "       `////////////////////////////:.            --::-----...-/    ",
+    "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+    "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/mm`   ",
+    "           ::::://::://::::::::::::::----------..-:....`.../Nmhd+o/ ",
+    "            -/:::-:::::---://:-::-::::----::---.-.......`-/oNN   `` ",
+    "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+    "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+    "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+    "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+    "                        .-:mNdhh:.......--::::-`                    ",
+    "                           yNh/..------..`                          ",
+}
+]]
