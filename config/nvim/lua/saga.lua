2cc7715002b2130888b7726aca3b6b854fa58d72
nytouu <capitaine7312@gmail.com>
2021-06-13 02:23:11 +0200

    nvim - moved saga cfg

--- config/nvim/lua/saga.lua
+++ config/nvim/lua/saga.lua
@@ -1,6 +1,10 @@
 require'lspsaga'.init_lsp_saga {
     use_saga_diagnostic_sign = true,
-    code_action_icon = "",
+    code_action_icon = "",
+    error_sign = "",
+    warn_sign = "",
+    hint_sign = "",
+    infor_sign = "",
     code_action_prompt = {
         enable = true,
         sign = true,
