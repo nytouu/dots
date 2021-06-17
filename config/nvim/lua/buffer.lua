e28271e0c2cd4ebe09df5471f5144cccfabee198
nytouu <capitaine7312@gmail.com>
2021-06-17 19:21:33 +0200

    nvim - bufferline config

--- config/nvim/lua/buffer.lua
+++ config/nvim/lua/buffer.lua
@@ -2,23 +2,26 @@ require('bufferline').setup{
 	options = {
 		buffer_close_icon = "",
         indicator_icon = '▎',
+        -- indicator_icon = "▊",
         modified_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
-        max_name_length = 18,
-        max_prefix_length = 15,
-        tab_size = 18,
-        enforce_regular_tabs = true,
-        view = "default",
+        max_name_length = 14,
+        max_prefix_length = 13,
+        tab_size = 20,
+        show_tab_indicators = true,
+        enforce_regular_tabs = false,
+        view = "multiview",
         show_buffer_close_icons = true,
 		show_close_icon = false,
-        separator_style = "thin",
-        offsets = {
-            {
-                filetype = "NvimTree",
-                text = "Files"
-            }
-        },
+        separator_style = "thin"
+        -- offsets = {
+        --     {
+        --         filetype = "NvimTree",
+        --         text = " ",
+        --         padding = 1
+        --     }
+        -- }
     },
     highlights = {
         background = {
