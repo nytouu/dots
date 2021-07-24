47307fdcced46612dba787997449c11cab50a584
nytouu <capitaine7312@gmail.com>
2021-07-25 00:56:29 +0200

    update hydra start script

--- local/share/hydra/autostart.sh
+++ local/share/hydra/autostart.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-lxsession &
-mpd &
-dunst &
-killall zscroll ; mpcscroll &
-killall hydrablocks ; hydrablocks &
-picom --experimental-backends &
-xclickroot -r rootclick &
+$HOME/.screenlayout/tv\ extend.sh
+
+xset m 0 0 
+xset s off
+xset -dpms
+xset s noblank
+setxkbmap fr
 nitrogen --restore
+
+/usr/bin/pipewire &
+
+hydrablocks &
+picom &
+mpd &
+mpcscroll &
+dunst &
+lxpolkit &
 # xwinwrap -g 1920x1080+0+0 -b -ov -fs -nf -ni -fdt -d -- mpv -wid WID /home/nytou/pics/gifs/bg.gif --loop --keepaspect=no &
 discord &
-setxkbmap fr
-xset m 0 0 
-sleep 3 && pkill -RTMIN+10 hydrablocks ; xset s 0 0
+sxhkd &
+otd &
+unclutter -idle 15 &
+sleep 3 && pkill -RTMIN+10 hydrablocks
